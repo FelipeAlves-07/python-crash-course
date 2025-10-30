b71f26392eea243a768298439292e306ba4efa9b
FelipeSantos0710 <felipefarias811@gmail.com>
2025-10-29 23:56:01 -0300

    create famous_quote_v2.py

---
+++ study/variables_and_data_types/famous_quote_v2.py
@@ -0,0 +1,5 @@
+famous_person = "Sócrates"
+famous_quote = "Conhece-te a ti mesmo e conhecerás o universo e os deuses."
+message = f'{famous_person} uma vez disse: "{famous_quote}"'
+
+print(message)
