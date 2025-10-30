225089467a0bda603fcbea3eb5763a347d4f4512
FelipeSantos0710 <felipefarias811@gmail.com>
2025-10-29 23:57:31 -0300

    create stripping_names.py

---
+++ study/variables_and_data_types/stripping_names.py
@@ -0,0 +1,13 @@
+person_name = "\tFelipe\n"
+
+print("Sem formatação:")
+print(person_name)
+
+print("Sem espaços adicionais à esquerda:")
+print(person_name.lstrip())
+
+print("Sem espaços adicionais à direita:")
+print(person_name.rstrip())
+
+print("Sem nenhum espaço adicional:")
+print(person_name.strip())
