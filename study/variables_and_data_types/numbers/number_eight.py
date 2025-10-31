fb121fd40ff05e9709faeaee2a14e53569bc85f4
FelipeSantos0710 <felipefarias811@gmail.com>
2025-10-30 23:28:22 -0300

    chore: add comments to file_extensions.py and number_eight.py

--- study/variables_and_data_types/numbers/number_eight.py
+++ study/variables_and_data_types/numbers/number_eight.py
@@ -1,3 +1,4 @@
+# Esse código tem como intuito usar as quatro principais operações matemáticas e retornar o número 8 como resultado de cada uma delas.
 print(5+3)
 print(12-4)
 print(4*2)
--- study/variables_and_data_types/strings/file_extensions.py
+++ study/variables_and_data_types/strings/file_extensions.py
@@ -1,3 +1,4 @@
+# Esse código tem como intuito exibir nomes de arquivos de texto sem a extensão .txt
 filename = "python_notes.txt"
 
 print(f"Nome do arquivo: {filename.removesuffix(".txt")}")
