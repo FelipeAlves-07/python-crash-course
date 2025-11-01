f6ac84d4c9b430a0e97daba4252b3d116df982ef
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-01 19:45:55 -0300

    add exercise 3.9: dinner guests count

---
+++ lists/3.9_dinner_guests_count.py
@@ -0,0 +1,8 @@
+guests = ["guilherme", "matheus", "lucas"]
+invitation_message = "estou organizando um jantar para o próximo sábado as 20h, e estou enviando essa mensagem como convite. Nos vemos lá!"
+
+print(f"Olá {guests[0].title()}, {invitation_message}")
+print(f"Olá {guests[1].title()}, {invitation_message}")
+print(f"Olá {guests[2].title()}, {invitation_message}")
+
+print(f"\nO número total de convidados é {len(guests)}")
