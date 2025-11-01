7fc92c265f48db907620f160275ba5f74df3e677
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-01 19:45:51 -0300

    add exercise 3.7: shrinking guest list

---
+++ lists/3.7_shrinking_guest_list.py
@@ -0,0 +1,63 @@
+guests = [
+    "guilherme",
+    "matheus",
+    "lucas"
+]
+invitation_message = "estou organizando um jantar para o próximo sábado as 20h, e estou enviando essa mensagem como convite. Nos vemos lá!"
+
+print(f"Olá {guests[0].title()}, {invitation_message}")
+print(f"Olá {guests[1].title()}, {invitation_message}")
+print(f"Olá {guests[2].title()}, {invitation_message}")
+
+print(f"\nInfelizmente {guests[2].title()} não poderá comparecer.\n")
+
+# Substituição do convidado indisponível
+guests[2] = "everton"
+
+print(f"Olá {guests[0].title()}, {invitation_message}")
+print(f"Olá {guests[1].title()}, {invitation_message}")
+print(f"Olá {guests[2].title()}, {invitation_message}")
+
+print("\nConsegui uma mesa maior, portanto consigo convidar mais 3 pessoas!\n")
+
+# Inserindo convidado no início da lista
+guests.insert(0, "kauan")
+# Inserindo convidado no meio da lista
+guests.insert(2, "pedro")
+# Inserindo convidado no final da lista
+guests.append("enzo")
+
+print(f"Olá {guests[0].title()}, {invitation_message}")
+print(f"Olá {guests[1].title()}, {invitation_message}")
+print(f"Olá {guests[2].title()}, {invitation_message}")
+print(f"Olá {guests[3].title()}, {invitation_message}")
+print(f"Olá {guests[4].title()}, {invitation_message}")
+print(f"Olá {guests[5].title()}, {invitation_message}")
+
+print("\nA mesa maior não chegará a tempo, portanto só vou conseguir convidar 2 pessoas!\n")
+
+removed_guests_message = "infelizmente vou precisar remover seu convite, assim que a mesa chegar eu marco uma nova data e te convido novamente!"
+
+popped_guest = guests.pop()
+print(f"{popped_guest.title()}, {removed_guests_message}")
+
+popped_guest = guests.pop()
+print(f"{popped_guest.title()}, {removed_guests_message}")
+
+popped_guest = guests.pop(2)
+print(f"{popped_guest.title()}, {removed_guests_message}")
+
+popped_guest = guests.pop(0)
+print(f"{popped_guest.title()}, {removed_guests_message}\n")
+
+
+remaining_guests_message = "você ainda está convidado, nos vemos no sábado!"
+
+print(f"{guests[0].title()}, {remaining_guests_message}")
+print(f"{guests[1].title()}, {remaining_guests_message}\n")
+
+del guests[0]
+del guests[0]
+
+# Mostra a lista para garantir que está vazia
+print(guests)
