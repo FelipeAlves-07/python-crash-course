bb4f6bda92fb2b66d34b2920cf9e03e6f64536dc
FelipeSantos0710 <felipefarias811@gmail.com>
2025-10-31 23:58:44 -0300

    create my_own_list.py

---
+++ lists/my_own_list.py
@@ -0,0 +1,9 @@
+games = [
+    "Ori and the Blind Forest",
+    "Ori and the Will of the Whisps",
+    "Hollow Knight Silksong"
+]
+
+print(f"Eu zerei o jogo {games[0]} por volta de agosto de 2025!")
+print(f"Eu começei a jogar {games[1]} em agosto de 2025 mas ainda não terminei, pretendo voltar a jogar após zerar o {games[2]}")
+print(f"Eu começei a jogar {games[2]} em setembro de 2025 e pretendo zerar até novembro de 2025!")
