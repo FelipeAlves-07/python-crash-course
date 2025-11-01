7bb074087240bf3370a63c0c688a6c03eef41dcb
FelipeSantos0710 <felipefarias811@gmail.com>
2025-10-31 23:37:26 -0300

    update names.py to show greeting messages instead of only the names

--- lists/names.py
+++ lists/names.py
@@ -1,6 +1,6 @@
 friend_names = ["guilherme", "matheus", "lucas", "everton"]
 
-print(friend_names[0].title())
-print(friend_names[1].title())
-print(friend_names[2].title())
-print(friend_names[3].title())
+print(f"Olá {friend_names[0].title()}, como você está?")
+print(f"Olá {friend_names[1].title()}, como você está?")
+print(f"Olá {friend_names[2].title()}, como você está?")
+print(f"Olá {friend_names[3].title()}, como você está?")
