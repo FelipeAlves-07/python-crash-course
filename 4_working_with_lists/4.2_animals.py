7f9a7f6a4744474703da9053c4e19cf06be0fa15
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-02 18:27:53 -0300

    add exercise 4.2: animals

---
+++ 4_working_with_lists/4.2_animals.py
@@ -0,0 +1,10 @@
+animals = [
+    "cachorro",
+    "gato",
+    "hamster"
+]
+
+for animal in animals:
+    print(f"Um {animal.title()} seria uma boa opção para um pet!")
+
+print("\nTodos esses animais seriam boas opções de pets!")
