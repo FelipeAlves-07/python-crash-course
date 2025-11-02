920b01d32133f2ab3877ec746ef6dfae993ebf81
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-02 18:27:50 -0300

    add exercise 4.1: pizzas

---
+++ 4_working_with_lists/4.1_pizzas.py
@@ -0,0 +1,11 @@
+flavours = [
+    "pepperoni",
+    "calabresa",
+    "frango e catupiri",
+    "mussarela"
+]
+
+for flavour in flavours:
+    print(f"Eu gosto de pizza de {flavour.title()}!")
+
+print("\nEu gosto muito de pizzas!")
