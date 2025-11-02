fe69725c3757ad59100ad3aaf0b787fd30716064
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-02 18:28:01 -0300

    add exercise 4.5: summing a million

---
+++ 4_working_with_lists/4.5_summing_a_million.py
@@ -0,0 +1,5 @@
+values = list(range(1,1000001))
+
+print(f"Menor valor da lista:\n{min(values)}\n")
+print(f"Maior valor da lista:\n{max(values)}\n")
+print(f"Soma dos valores da lista:\n{sum(values)}\n")
