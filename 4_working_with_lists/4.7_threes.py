187e0b6a48b5b0ea893baf480e18e3c68794cec3
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-02 18:28:06 -0300

    add exercise 4.7: threes

---
+++ 4_working_with_lists/4.7_threes.py
@@ -0,0 +1,6 @@
+# Um programa que gera uma lista com os múltiplos de 3 até 30 e exibe cada um dos valores na tela
+
+multiples_of_three = list(range(3,31,3))
+
+for multiples in multiples_of_three:
+    print(multiples)
