0137d3308215a1c2f294b300a2b34ca28fbe66c2
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-02 18:28:14 -0300

    add exercise 4.10: slices

---
+++ 4_working_with_lists/4.10_slices.py
@@ -0,0 +1,7 @@
+cubes = [value**3 for value in range(1,12)]
+
+print(f"Lista completa:\n{cubes}")
+
+print(f"\nOs três primeiros valores da lista são:\n{cubes[:3]}")
+print(f"\nOs três valores do meio da lista são:\n{cubes[4:7]}")
+print(f"\nOs três últimos valores da lista são:\n{cubes[-3:]}")
