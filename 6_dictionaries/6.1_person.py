5154de9653c8b84599d1776947cc275d45fb933e
FelipeSantos0710 <felipefarias811@gmail.com>
2025-11-29 14:13:58 -0300

    add exercises 6.1 and 6.2

---
+++ 6_dictionaries/6.1_person.py
@@ -0,0 +1,11 @@
+person_0 = {
+    'first_name': 'felipe',
+    'last_name': 'santos',
+    'age': 23,
+    'city': 'sao paulo'
+    }
+
+print(f"Primeiro nome: {person_0['first_name'].title()}")
+print(f"Último nome: {person_0['last_name'].title()}")
+print(f"Idade: {person_0['age']}")
+print(f"Cidade: {person_0['city'].title()}")
---
+++ 6_dictionaries/6.2_favorite_numbers.py
@@ -0,0 +1,15 @@
+favorite_numbers = {
+    'caique': 8,
+    'enzo': 7,
+    'renan': 27,
+    'guilherme': 5,
+    'everton': 4
+}
+
+print(f"Lista de pessoas e seus números favoritos:")
+print(f"Caique: {favorite_numbers['caique']}")
+print(f"Enzo: {favorite_numbers['enzo']}")
+print(f"Renan: {favorite_numbers['renan']}")
+print(f"Guilherme: {favorite_numbers['guilherme']}")
+print(f"Everton: {favorite_numbers['everton']}")
+
