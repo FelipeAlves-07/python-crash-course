b195b966c7a687ddf57f853ed97c0a7f6f63598e
FelipeSantos0710 <felipefarias811@gmail.com>
2026-01-15 23:48:12 -0300

    add exercises from 8.1 to 8.2

---
+++ 8_functions/8.1_message.py
@@ -0,0 +1,5 @@
+def display_message():
+    """Mostra o que está sendo aprendido neste capítulo"""
+    print('Estou aprendendo sobre funções em Python!!')
+
+display_message()
---
+++ 8_functions/8.2_favorite_book.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    """Mostra um dos livros favoritos do usuário"""
+    print(f'Um dos meus livros favoritos chama-se {title}')
+
+favorite_book('Python Crash Course')
